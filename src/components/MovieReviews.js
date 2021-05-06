5ee2a25cd82b8a0a05e5120f51737b351ca634d1
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-05-06 23:20:00 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,11 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = ({reviews}) => {
+  return (
+    <div className="review">
+      <h2>{review.display_title}</h2>
+      <h3>{review.headline}</h3>
+      <p>{review.summary_short}</p>
+    </div>
+  )
+}
